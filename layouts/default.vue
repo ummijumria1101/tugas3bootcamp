167376f7795d21130bf2d6d064a4aaad167dea5b
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-17 23:18:34 +0700

    update default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,26 @@
 <template>
-    <div>
-        
-            <header class="shadow-sm bg-txtprimary">
-                <nav class="text-xl text-secondarycolor font-semibold p-4 flex bg-primarycolor">
-                    <ul class="flex gap-4">
-                        <li><NuxtLink to="/">Home</NuxtLink></li>
-                        <li><NuxtLink to="about">About</NuxtLink></li>
-                        <li><NuxtLink to="contact">Contact</NuxtLink></li>
-                    </ul>
-                </nav>
-            </header>
-
-        <slot />
-            <div class="flex shadow-xl place-content-center bg-txtbase">
-                <footer class="max-w-sm text-center overflow-hidden font-semibold text-txtprimary">
-            <h3 class="">© 2023 Ummi Jumria, Bootcamp Juke Technology. All rights reserved</h3>
-            </footer>
-            </div>
+        <div>        
+                <header class="shadow-sm bg-txtprimary">
+                    <nav class="text-xl text-secondarycolor font-semibold p-4 flex bg-primarycolor">
+                        <ul class="flex gap-4">
+                            <li><NuxtLink to="/">Home</NuxtLink></li>
+                            <li><NuxtLink to="about">About</NuxtLink></li>
+                            <li><NuxtLink to="contact">Contact</NuxtLink></li>
+                        </ul>
+                    </nav>
+                </header>
             
-    </div>
+                <slot />
+                   
+                <div class="flex shadow-xl place-content-center bg-txtbase">
+                    <footer class="max-w-sm text-center overflow-hidden font-semibold text-txtprimary">
+                <h3 class="">© 2023 Ummi Jumria, Bootcamp Juke Technology. All rights reserved</h3>
+                </footer>
+                </div>  
+        </div>
 </template>
 
 
-<style scoped>
-    .container{
-        color: #F1E1AA;
-        font-weight: bold;
-        font-size: larger;
-        /* width:  */
-    }
-    
+<style scoped>    
 
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,23 @@
-<template class="">
-    <div class="bg-txtprimary">     
-        <!-- <h2 class="text-4xl text-center font-semibold">Tentang Website</h2>  -->
-        <div class="wrapper justify-center inline-block align-middle">
-            <div class=" bg-txtprimary space-x-10 object-cover h-300 w-full">
-                <img class="item-center " src = "https://i.postimg.cc/PqqVzdht/aboutus.png">
-            </div>
-            <div class="mr-100 bg-txtprimary text-primarycolor">
-                <h3 class="card text-4xl text-center font-semibold space-y-80">Tentang Website</h3>
-                <p class="card1 space-x-10 text-justify">Website ini dibuat sebagai bagian dari tugas bootcamp yang diadakan oleh PT Juke Teknologi. Dalam rangkaian program bootcamp ini, peserta diberikan kesempatan untuk mempelajari dan mengembangkan keterampilan dalam pengembangan website. Dengan membangun website ini, peserta dapat mengaplikasikan pengetahuan yang telah mereka peroleh dan memperlihatkan kemampuan mereka dalam merancang dan membangun sebuah situs web fungsional. Tujuan dari pembuatan website ini adalah untuk memberikan kesempatan kepada peserta untuk berlatih dan memperluas pemahaman mereka tentang teknologi web, serta untuk memperkuat keterampilan mereka dalam menghadapi tantangan pengembangan perangkat lunak.</p>
+<template>
+    <div class="flex bg-txtprimary">
+        <div class="flex">
+            <div class="p-4 max-w-50 w-1/2">
+            <img src="~/assets/img/aboutus.png"/>
+        </div>
+        <div class="pt-10 px-10 max-h-screen w-1/2 drop-shadow-xl">
+            <div class="mr-100 text-primarycolor">
+                <h3 class="text-4xl text-center font-semibold space-y-80">Tentang Website</h3>
+                <p class="space-x-10 text-justify">Website ini dibuat sebagai bagian dari tugas bootcamp yang diadakan oleh PT Juke Teknologi. Dalam rangkaian program bootcamp ini, peserta diberikan kesempatan untuk mempelajari dan mengembangkan keterampilan dalam pengembangan website. Dengan membangun website ini, peserta dapat mengaplikasikan pengetahuan yang telah mereka peroleh dan memperlihatkan kemampuan mereka dalam merancang dan membangun sebuah situs web fungsional. Tujuan dari pembuatan website ini adalah untuk memberikan kesempatan kepada peserta untuk berlatih dan memperluas pemahaman mereka tentang teknologi web, serta untuk memperkuat keterampilan mereka dalam menghadapi tantangan pengembangan perangkat lunak.</p>
             </div>
         </div>
     </div>
     <div>
-        
-    </div>
+        </div>
+
+        </div>
+
+    
+
     
 </template>
 
@@ -25,52 +29,6 @@
 </script>
 
 <style lang="scss" scoped>
-   /* h2 {
-        margin-bottom: 20px;
-        font-size: 20px;
-    }  */
-   /* p{
-        margin: 20px 0;
-    } */
-
-    *,
-html, 
-body{
-    margin:0;
-    padding: 0;
-    font-family: 'Poppins', sans-serif;
-   
-
-}
-
-
-.card{
-    margin-top: 30px;
-    margin-bottom: 20px;
-   // text-size-adjust: 50px;
-    /* padding: 32px;
-    margin:8px; */
-}
-
-.card1{
-    margin-right: 50px;
-}
-
-.wrapper{
-    display: flex;
-    justify-content: center;
-    // width: auto;
-   height: 100vh;
-   padding-top: 100px;
-   background-color: #F1E1AA ;
-   // text-align: center; 
-   // vertical-align:20px;
-  //  margin-top: 50px;
-    // margin-left:
-   // margin: 100px;  
-    //margin-right: 100px;
-  
-    
-}
+//
 
 </style>
