7aff3f01126e60cbf57e560934258877b7b604f3
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-17 12:53:05 +0700

    update tugas 6

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="flex h-screen bg-txtprimary place-content-center">
+    <!-- <div class="flex h-screen bg-txtprimary place-content-center">
         <div class="max-w-xl text-justify shadow-xl p-4 my-auto">
         <p class="font-bold text-center text-xl">Ini adalah routing dari {{ [id] }}</p>
         <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque neque placeat necessitatibus illum eum repellendus iure id qui quidem cupiditate quae quam, obcaecati rerum commodi ut laboriosam enim nemo autem vel eaque. Praesentium fuga quis blanditiis a expedita laboriosam fugit? Sequi in illum aspernatur. Et dolor perspiciatis fuga qui dolorem.</p>
     </div>
+    </div> -->
 
+    <div class="flex h-screen bg-txtprimary place-content-center">
+        <div class="max-w-xl text-justify shadow-xl p-4 my-auto">
+      <main v-if="$route.params.id">
+        <h1 class="font-bold text-center text-xl" v-text="'Ini adalah routing dari ' + $route.params.id"></h1>
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque neque placeat necessitatibus illum eum repellendus iure id qui quidem cupiditate quae quam, obcaecati rerum commodi ut laboriosam enim nemo autem vel eaque. Praesentium fuga quis blanditiis a expedita laboriosam fugit? Sequi in illum aspernatur. Et dolor perspiciatis fuga qui dolorem.</p>
+      </main>
+      <main v-else>
+        <h1>lalala</h1>
+      </main>
     </div>
+    </div>
+    
    
 </template>
 
 <script setup>
-const {  } = useRoute().params
+// const {  } = useRoute().params
+// //console.log(route.params.id)
 
 </script>
 
 <style scoped>
 
 </style>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
       'secondarycolor' : '#F1E1AA', //abu muda
       'txtprimary' : '#F1E1AA', //abu tua
       'txtsecondary' : '#F1E1AA', // hitam
-      'txtbase' : '#1A1A1A'
+      'txtbase' : '#1A1A1A',
+      'sidecolor' : '#e0ad75'
       
 
     },
