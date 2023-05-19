56723e8eb0ef939b607672584a6ae196c980e297
Ummi Jumria <58371327+ummijumria1101@users.noreply.github.com>
2023-05-19 19:38:28 +0700

    update

--- pages/page4.vue
+++ pages/page4.vue
@@ -1,72 +1,19 @@
 <template>
-    <div class="flex">
-        <div class="bg-txtprimary">      
-        <h1 class="text-center text-4xl font-semibold text-primarycolor p-4">Tabel Data Antena</h1>
-        <table>
-            <thead>
-                <tr class=""> <!-- table row -->
-                    <th class="p-10">Nama Antena</th>
-                    <th class="p-10">Deskripsi</th>
-                    <th class="p-10">Stok</th>
-                    <th class="p-10">Harga Pasaran</th>
-                    <th class="p-10">Diskon</th>
-                    <th class="p-10">Harga Setelah Diskon</th>
-                    <th class="p-10">Image</th>
-                </tr>
-            </thead>
-            <tbody id="data-output">
-                <div class="products">
-                    
-                </div>
-
-            </tbody>
-        </table>
-        <!-- <img class="image mx-auto flex item-center" src = "https://i.postimg.cc/G28cdcB5/Suppression-of-Cross-Polarization-of-the-Microstrip-Integrated-Balun-Fed-Printed-Dipole-Antenna.jpg"> -->
-        <div class="ml-10 mr-10 inline-block">
-            <p class="text-primarycolor text-justify p-4">Antena adalah perangkat yang biasanya terbuat dari logam (sebagai tongkat atau kawat) untuk memancarkan dan menerima gelombang radio. Antena adalah komponen utama dalam sistem WLAN. Antena bekerja dengan memancarkan gelombang elektromagnetik dalam arah radial yang terkoordinasi. Antena sangat diperlukan terutama dalam teknologi telekomunikasi saat ini. Antena terdiri dari berbagai jenis, terdapat antena horn, antena yagi uda, antena mikrostrip dan lain lain.</p>
-        </div>
-
-        <h3 class="ml-10 mr-10 inline-block font-semibold text-primarycolor pl-4">Berikut adalah tutorial membuat salah satu jenis antena yaitu antena mikrostrip:</h3>
-        <ul class="ml-10 mr-10 inline-block mb-50 list-decimal text-primarycolor pb-8 pl-8">
-            <li>Tentukan jenis patch yang akan dibuat</li>
-            <li>Buat spesifikasi antena yang diinginkan</li>
-          <li>Menghitung dimensi antena</li>
-          <li>Mendesain antena di software CST</li>
-          <li>Mencoba simulasi antena di software CST</li>
-          <li>Memeriksa spesifikasi antena seperti VSWR dan return loss apakah sudah sesuai atau belum</li>
-          <li>Jika belum sesuai optimasi dimensi antena di CST</li>
-          <li>Fabrikasi antena yang spesifikasinya sudah sesuai di CST</li>
-          <li>Mengukur antena yang sudah di fabrikasi dengan Vector Network Analyzer (VNA), pastikan spesifikasi antena sudah sesuai</li>
-          <li>Antena telah jadi</li>   
-          </ul>     
-
-        </div>
-        <div class="flex-col bg-txtprimary drop-shadow-xl max-w-fit max-h-50">
-            <div>
-                <p class="text-justify p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores excepturi mollitia quidem adipisci, sapiente enim quis dignissimos vero necessitatibus soluta!</p>
-            </div>
-            <div>
-                <p class="text-justify p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores excepturi mollitia quidem adipisci, sapiente enim quis dignissimos vero necessitatibus soluta!</p>
-            </div>
-            <div>
-                <p class="text-justify p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores excepturi mollitia quidem adipisci, sapiente enim quis dignissimos vero necessitatibus soluta!</p>
-            </div>
-            <div>
-                <p class="text-justify p-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores excepturi mollitia quidem adipisci, sapiente enim quis dignissimos vero necessitatibus soluta!</p>
-            </div>
-
-        </div>
-
+    <div>
+        <p v-for="user in users">{{users}}</p>
     </div>
-
-        
-
-    
 </template>
 
-<script src="">
-//
+<script>
+    import json from '~/asset/users.json'
 
+    export default {
+        data() {
+            return { 
+                users: json
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
